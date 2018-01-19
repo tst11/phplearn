bb072db4180969c394fce2a52b6b0053840ebbb8
Tadas Stasiulionis <tadas.stasiulionis@gmail.com>
2018-01-19 15:33:20 +0200

    Simple syntax.

--- chapter3/name.php
+++ chapter3/name.php
@@ -2,8 +2,16 @@
 
     $firstName = $_REQUEST['firstname'];
     $lastName = $_REQUEST['lastname'];
-    echo 'Welcome to our website, ' .
-    htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
-    htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+    // $name = $_REQUEST['name'];
+    if ($firstName == 'Kevin' and $lastName == 'Yank') {
+        echo 'Welcome, oh glorious leader!';
+    } else {
+        echo 'Welcome to our website, ' . 
+        htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+        htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+    }
+    // echo 'Welcome to our website, ' .
+    // htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+    // htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
 
 ?>
